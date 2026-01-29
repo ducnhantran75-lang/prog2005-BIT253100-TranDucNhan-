6fa6378b68f2d7bd72c10d5aa056b0d4164a2ba6
ducnhantran75-lang <ducnhantran75@gmail.com>
2026-01-29 23:04:05 +0700

    Create Ex5.py

---
+++ Ex5.py
@@ -0,0 +1,14 @@
+name = "An"
+age = 18
+average_score = 7.5
+
+print(type(name))
+print(type(age))
+print(type(average_score))
+
+age_next_year = age + 1
+doubled_score = average_score * 2
+
+print("Tên:", name)
+print("Tuổi năm sau:", age_next_year)
+print("Điểm nhân đôi:", doubled_score)
