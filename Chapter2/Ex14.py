a12d7c2e7974afa1d60350261348cd2ee3da4c2e
ducnhantran75-lang <ducnhantran75@gmail.com>
2026-02-05 20:48:06 +0700

    Create Ex14.py

---
+++ Chapter2/Ex14.py
@@ -0,0 +1,11 @@
+n = int(input("Nhập n: "))
+
+count = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        count += 1
+
+if count == 2:
+    print("Là số nguyên tố")
+else:
+    print("Không phải số nguyên tố")
