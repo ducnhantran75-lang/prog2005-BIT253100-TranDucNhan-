41e723ac006b6cbcaa3ef5cb0c85f3a08002faa5
ducnhantran75-lang <ducnhantran75@gmail.com>
2026-03-05 15:45:45 +0700

    Create Ex11.py

---
+++ Chapter3/Ex11.py
@@ -0,0 +1,7 @@
+numbers = list(map(int, input("Nhap danh sach so: ").split()))
+
+max_value = max(numbers)
+min_value = min(numbers)
+
+print("Gia tri lon nhat:", max_value)
+print("Gia tri nho nhat:", min_value)
