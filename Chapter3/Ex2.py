727ae942e45b0989afd5a10454d3a44a7bdd1796
ducnhantran75-lang <ducnhantran75@gmail.com>
2026-03-05 15:41:25 +0700

    Create Ex2.py

---
+++ Chapter3/Ex2.py
@@ -0,0 +1,9 @@
+friends = ["Nam", "Trung", "Nhan"]
+
+print("Danh sach ban dau:", friends)
+
+friends.append("Cuong")
+print("Sau khi them ban:", friends)
+
+friends.pop(1)
+print("Sau khi xoa ban thu hai:", friends)
