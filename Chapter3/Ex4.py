d1d4a385180753fe31ba00c6d94ac97cf832158d
ducnhantran75-lang <ducnhantran75@gmail.com>
2026-03-05 15:42:23 +0700

    Create Ex4.py

---
+++ Chapter3/Ex4.py
@@ -0,0 +1,10 @@
+numbers = [3, 5, 1, 5, 9, 2]
+
+numbers.sort()
+print("Sap xep tang dan:", numbers)
+
+numbers.reverse()
+print("Dao nguoc:", numbers)
+
+count_5 = numbers.count(5)
+print("So lan xuat hien cua 5:", count_5)
