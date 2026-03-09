5ae84f5625f555e48e39c4ed589e7b8fb2812366
ducnhantran75-lang <ducnhantran75@gmail.com>
2026-03-09 17:35:26 +0700

    Create bài 1.py

---
+++ kiểm tra giữa kì /bài 1.py
@@ -0,0 +1,14 @@
+import math
+
+# nhập dữ liệu
+a = float(input("Nhập a: "))
+b = float(input("Nhập b: "))
+c = float(input("Nhập c: "))
+
+# tìm số lớn nhất và nhỏ nhất
+max_num = max(a, b, c)
+min_num = min(a, b, c)
+
+print("Số lớn nhất là:", max_num)
+print("Số nhỏ nhất là:", min_num)
+
