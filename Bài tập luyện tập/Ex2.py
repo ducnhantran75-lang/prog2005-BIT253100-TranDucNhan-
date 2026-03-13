5625ea6202343bc2071803a1aab525e1589ec018
ducnhantran75-lang <ducnhantran75@gmail.com>
2026-03-13 17:34:20 +0700

    Create Ex2.py

---
+++ Bài tập luyện tập/Ex2.py
@@ -0,0 +1,10 @@
+import math
+
+a = float(input("Nhap so thu nhat:"))
+b = float(input("Nhap so thu hai:"))
+
+print("Luy thua:", a ** b )
+print("Can bac 2 cua a:", math.sqrt(a))
+print("Chia lay phan nguyen:", a // b )
+print("chia lay phan du:", a % b)
+print("Lam tron so:", round (a))
